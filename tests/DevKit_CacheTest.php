7367ef402642ac512fe4be9a865c3d5048d1673f
ArtemKo7v <artemk@211.ru>
2015-04-10 20:24:35 +0700

    Mistype fixed

--- tests/DevKit_CacheTest.php
+++ tests/DevKit_CacheTest.php
@@ -2,7 +2,7 @@
 
 use \AmiLabs\DevKit\Cache;
 
-define('PATH_TMP', rtim(sys_get_temp_dir(), '/'));
+define('PATH_TMP', rtrim(sys_get_temp_dir(), '/'));
 
 require_once __DIR__ . '/../src/AmiLabs/DevKit/Cache.php';
 
