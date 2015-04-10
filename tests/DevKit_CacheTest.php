a6bbc9f1815550f213ae772e119299c4d0177e79
ArtemKo7v <artemk@211.ru>
2015-04-10 21:13:54 +0700

    DocBlocks, test for clearIfOlderThan

--- tests/DevKit_CacheTest.php
+++ tests/DevKit_CacheTest.php
@@ -7,9 +7,23 @@ define('PATH_TMP', rtrim(sys_get_temp_dir(), '/'));
 require_once __DIR__ . '/../src/AmiLabs/DevKit/Cache.php';
 
 class DevKit_CacheTest extends PHPUnit_Framework_TestCase{
-
+    /**
+     * Random cache id
+     *
+     * @var string
+     */
     protected $cacheFile;
 
+    /**
+     * Test data to cache
+     *
+     * @var array
+     */
+    protected $data = array('test' => array('a1' => array('b1', 'b2', 'b3'), 'a2' => 0.0001, 'a3' => TRUE));
+
+    /**
+     * Constructor. Prepares cache.
+     */
     public function __construct(){
         parent::__construct();
         $this->cacheFile = md5(time());
@@ -18,17 +32,25 @@ class DevKit_CacheTest extends PHPUnit_Framework_TestCase{
         @unlink($this->cacheFile);
     }
 
+    /**
+     * @covers \AmiLabs\DevKit\Cache::get
+     */
     public function testCache(){
         $oCache = Cache::get($this->cacheFile);
         $this->assertEquals('AmiLabs\DevKit\FileCache', get_class($oCache));
         $this->assertEquals(FALSE, file_exists($oCache->getFilename()));
     }
 
+    /**
+     * @covers \AmiLabs\DevKit\FileCache::exists
+     * @covers \AmiLabs\DevKit\FileCache::save
+     * @covers \AmiLabs\DevKit\FileCache::clear
+     */
     public function testExistsSaveClear(){
         $oCache = Cache::get($this->cacheFile);
         $this->assertEquals(FALSE, $oCache->exists());
         $this->assertEquals(FALSE, file_exists($oCache->getFilename()));
-        $oCache->save('test data');
+        $oCache->save($this->data);
         $this->assertEquals(TRUE, $oCache->exists());
         $this->assertEquals(TRUE, file_exists($oCache->getFilename()));
         $oCache->clear();
@@ -36,11 +58,13 @@ class DevKit_CacheTest extends PHPUnit_Framework_TestCase{
         $this->assertEquals(FALSE, file_exists($oCache->getFilename()));
     }
 
+    /**
+     * @covers \AmiLabs\DevKit\FileCache::save
+     */
     public function testSave(){
         $oCache = Cache::get($this->cacheFile);
-        $data = array('test' => array('a1' => array('b1', 'b2', 'b3'), 'a2' => 0.0001, 'a3' => TRUE));
-        $serialized = serialize($data);
-        $oCache->save($data);
+        $serialized = serialize($this->data);
+        $oCache->save($this->data);
         $this->assertEquals(TRUE, file_exists($oCache->getFilename()));
         $readed = file_get_contents($oCache->getFilename());
         $this->assertEquals($serialized, $readed);
@@ -48,10 +72,12 @@ class DevKit_CacheTest extends PHPUnit_Framework_TestCase{
         $this->assertEquals(FALSE, file_exists($oCache->getFilename()));
     }
 
+    /**
+     * @covers \AmiLabs\DevKit\FileCache::load
+     */
     public function testLoad(){
         $oCache = Cache::get($this->cacheFile);
-        $data = array('test' => array('a1' => array('b1', 'b2', 'b3'), 'a2' => 0.0001, 'a3' => TRUE));
-        $serialized = serialize($data);
+        $serialized = serialize($this->data);
         file_put_contents($oCache->getFilename(), $serialized);
         chmod($oCache->getFilename(), 0777);
         $data = $oCache->load();
@@ -65,7 +91,18 @@ class DevKit_CacheTest extends PHPUnit_Framework_TestCase{
         $this->assertEquals(FALSE, file_exists($oCache->getFilename()));
     }
 
+    /**
+     * @covers \AmiLabs\DevKit\FileCache::clearIfOlderThan
+     */
     public function testClearIfOlderThan(){
-
+        $oCache = Cache::get($this->cacheFile);
+        $oCache->save($this->data);
+        sleep(1);
+        $oCache->clearIfOlderThan(3);
+        $this->assertEquals(TRUE, file_exists($oCache->getFilename()));
+        sleep(4);
+        $oCache->clearIfOlderThan(3);
+        $this->assertEquals(FALSE, file_exists($oCache->getFilename()));
+        $oCache->clear();
     }
 }
