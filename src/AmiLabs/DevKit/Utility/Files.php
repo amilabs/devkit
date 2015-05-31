dae1a5e17a566be7d4d8991f46ca93764c571c6d
Deepeloper <weregod@gmail.com>
2015-05-31 14:52:21 +0700

    Files utility class added.

---
+++ src/AmiLabs/DevKit/Utility/Files.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AmiLabs\DevKit\Utility;
+
+/**
+ * Files utility class.
+ *
+ * @package AmiLabs/DevKit/Utility
+ */
+class Files{
+    /**
+     * Saves file.
+     *
+     * @param  string $path     Path
+     * @param  string $content  Content
+     * @param  int    $mode     File mode ({@see http://php.net/manual/en/function.chmod.php})
+     * @param  int    $flags    Flags ({@see http://php.net/manual/en/function.file-put-contents.php})
+     * @return bool
+     */
+    public static function saveFile($path, $content, $mode = 0666, $flags){
+        $res = FALSE;
+        $res = file_put_contents($path, $content, $flags);
+        if($res){
+            @chmod($path, $mode);
+        }
+
+        return $res;
+    }
+}
