ba20068186752b1245fa650aeb1365bfffe69c33
Deepeloper <weregod@gmail.com>
2015-06-22 20:06:32 +0700

    sortMultiArrayPreserveKeys() method added.

--- src/AmiLabs/DevKit/Utility/Arrays.php
+++ src/AmiLabs/DevKit/Utility/Arrays.php
@@ -29,4 +29,37 @@ class Arrays{
 
         return FALSE;
     }
+
+    /**
+     * Sort an array by column preserving row keys.
+     *
+     * @param  array &$aArray       Array
+     * @param  int|string $sortCol  Sort column
+     * @param  int $sort            Sort type: SORT_STRING|SORT_REGULAR|SORT_NUMERIC
+     * @param  int $direction       Sort direction: SORT_ASC|SORT_DESC
+     * @return void
+     */
+    public static function sortMultiArrayPreserveKeys(
+        array &$aArray,
+        $sortCol,
+        $sort = SORT_STRING,
+        $direction = SORT_ASC
+    ){
+        if(!sizeof($aArray)){
+            return;
+        }
+        $aIndex = array();
+        $i = 0;
+        foreach($aArray as $key => $aRow){
+            $aIndex['pos'][$i]  = $key;
+            $aIndex['name'][$i] = $aRow[$sortCol];
+            ++$i;
+        }
+        array_multisort($aIndex['name'], $sort, $direction, $aIndex['pos']);
+        $aRes = array();
+        for($j = 0; $j < $i; $j++){
+            $aRes[$aIndex['pos'][$j]] = $aArray[$aIndex['pos'][$j]];
+        }
+        $aArray = $aRes;
+    }
 }
